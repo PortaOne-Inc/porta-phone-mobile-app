0837d119d61aa1207e3872f0d5dd2466e1d3d905
Dmitro Serdun <d.serdun@webtrit.com>
2023-03-03 15:05:56 +0200

    feat: add base folder structure

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:webtrit_configurator/main.dart';
+import 'package:webtrit_configurator/core/core.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MaterialApplication());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
