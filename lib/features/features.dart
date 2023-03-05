1d364dad759eeac1e7136c615dc6866cf63356cf
Dmitro Serdun <d.serdun@webtrit.com>
2023-03-05 18:23:35 +0200

    feat: add features export file

---
+++ lib/features/features.dart
@@ -0,0 +1,6 @@
+export 'auth/auth.dart';
+export 'theme_collection/theme_collection.dart';
+export 'theme_create/themes_create.dart';
+export 'theme_edit/theme_edit.dart';
+export 'vendor_collection/vendor_collection.dart';
+export 'vendor_create/vendor_create.dart';
