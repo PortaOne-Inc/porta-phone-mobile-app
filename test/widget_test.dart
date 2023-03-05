dbfbe0cb2d1f99bd68575a0671b381ec22e2aeae
Dmitro Serdun <d.serdun@webtrit.com>
2023-03-05 18:34:10 +0200

    feat: update imports in tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:webtrit_configurator/core/core.dart';
+import 'package:webtrit_configurator/core/config/app/application.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
