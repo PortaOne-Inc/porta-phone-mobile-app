abf8dacba756e467add415378ac8ea4a07c0bb31
Dmitro Serdun <d.serdun@webtrit.com>
2023-03-08 12:13:12 +0200

    refactor[firebase]: rename theme endpoint variable names

--- firebase/functions/src/controllers/themes.ts
+++ firebase/functions/src/controllers/themes.ts
@@ -1,26 +1,23 @@
 import {Request, Response} from "express"
 import {db} from '../config/firebase'
 
-
-const getThemesByApplicationId = async (req: Request, resp: Response) => {
-    const {applicationId, userId} = req.params
+const getUserDefaultTheme = async (req: Request, resp: Response) => {
+    const {userId, vendorId} = req.params
     try {
-        const database = (await db.ref(`/applications/${userId}/${applicationId}`).get()).val();
-        const databaseTheme = (await db.ref(`/themes/${userId}/${database["theme"]}`).get()).val();
+        const vendor = (await db.ref(`/applications/${userId}/${vendorId}`).get()).val();
+        const vendorDefaultThemeId = vendor["theme"];
+        const vendorThemesDefaultThemeObject = (await db.ref(`/themes/${userId}/${vendorId}/${vendorDefaultThemeId}`).get()).val();
+        const buffer = Buffer.from(JSON.stringify(vendorThemesDefaultThemeObject), 'binary');
 
-        const file = Buffer.from(JSON.stringify(databaseTheme), 'binary');
-
-        resp.writeHead(200, {
+        return resp.writeHead(200, {
             'Content-Type': "application/json",
             'Content-disposition': 'attachment;filename=' + "theme.json",
-            'Content-Length': file.length
-        });
-
-        return resp.end(file)
+            'Content-Length': buffer.length
+        }).end(buffer)
 
     } catch (error) {
         return resp.status(500).json(error)
     }
 }
 
-export {getThemesByApplicationId}
+export {getUserDefaultTheme}
--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -2,16 +2,14 @@ import * as functions from 'firebase-functions'
 import * as express from 'express'
 import * as bodyParser from "body-parser";
 
-import {getThemesByApplicationId} from './controllers/themes'
+import {getUserDefaultTheme} from './controllers/themes'
 
 const api = express();
 const router = express.Router();
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended: false}));
-
-router.get('/themes/:applicationId/:userId', (req, res) => getThemesByApplicationId(req, res));
-
+router.get('/user/:userId/vendor/:vendorId', (req, res) => getUserDefaultTheme(req, res));
 
 api.use('/v1', router);
 exports.api = functions.https.onRequest(api)
