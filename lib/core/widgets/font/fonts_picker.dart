a4b1b93957d09123b9ee5e337814911c1905f25e
Dmitro Serdun <d.serdun@webtrit.com>
2023-03-13 11:10:40 +0200

    feat: add pagination for font picker

--- lib/core/widgets/font/fonts_picker.dart
+++ lib/core/widgets/font/fonts_picker.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'package:url_launcher/url_launcher.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
 import 'package:webtrit_configurator/core/assets/assets.dart';
 import 'package:webtrit_configurator/core/config/l10n/l10n.dart';
-import 'package:webtrit_configurator/share/entity/entity.dart';
 
 import 'fonts.dart';
 
@@ -23,22 +23,50 @@ class FontsPicker extends StatefulWidget {
 }
 
 class _FontsPickerState extends State<FontsPicker> {
-  final _allGoogleStyles = <GoogleStyle>[];
-  final _filteredGoogleStyles = <GoogleStyle>[];
+  final _fonts = <String>[];
+  final _filteredFonts = <String>[];
 
-  final _renderedItemsStyleCount = 10;
+  var _loadedItems = 0;
+  late int _pageSize;
+
+  final PagingController<int, String> _pagingController = PagingController(firstPageKey: 0);
 
   @override
   void initState() {
-    _initStyles();
     super.initState();
+    _fonts.addAll(GoogleFonts.asMap().keys.toList());
+    _filteredFonts.addAll(_fonts);
   }
 
-  void _initStyles() {
-    GoogleFonts.asMap().forEach((key, value) {
-      _allGoogleStyles.add(GoogleStyle(key, value));
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _pageSize = MediaQuery.of(context).size.height ~/ 56;
+    _loadedItems = _pageSize;
+    _pagingController.addPageRequestListener((pageKey) {
+      _fetchPage(pageKey);
     });
-    _filteredGoogleStyles.addAll(_allGoogleStyles);
+  }
+
+  @override
+  void dispose() {
+    _pagingController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _fetchPage(int pageKey) async {
+    try {
+      final newItems = _filteredFonts.take(_loadedItems + _pageSize).toList();
+      final isLastPage = newItems.length < _pageSize;
+      if (isLastPage) {
+        _pagingController.appendLastPage(newItems);
+      } else {
+        final nextPageKey = pageKey + newItems.length;
+        _pagingController.appendPage(newItems, nextPageKey);
+      }
+    } catch (error) {
+      _pagingController.error = error;
+    }
   }
 
   @override
@@ -51,52 +79,47 @@ class _FontsPickerState extends State<FontsPicker> {
         children: [
           Container(
             margin: const EdgeInsets.symmetric(vertical: 16),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Expanded(
-                    flex: 0,
-                    child: InkWell(
-                      child: Container(
-                        margin: const EdgeInsets.only(left: 24),
-                        child: const Icon(Icons.arrow_back_ios),
-                      ),
-                      onTap: () => widget.onClose(),
-                    )),
-                Expanded(
-                  child: Container(
-                      margin: const EdgeInsets.only(left: 8, right: 24),
-                      child: TextField(
-                        decoration: const InputDecoration(
-                          labelText: 'Font name',
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.black12),
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.black12),
-                          ),
-                          border: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.black12),
-                          ),
-                        ),
-                        onChanged: (v) async {
-                          final filteredList = _allGoogleStyles.where((element) => element.name.contains(v)).toList();
-                          _filteredGoogleStyles.clear();
-                          _filteredGoogleStyles.addAll(filteredList);
-                          setState(() {});
-                        },
-                      )),
+            child: Container(
+              margin: const EdgeInsets.only(left: 16, right: 16),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Font name',
+                  prefixIcon: InkWell(
+                    child: Container(
+                      margin: const EdgeInsets.only(left: 16, right: 8),
+                      child: const Icon(Icons.arrow_back_ios),
+                    ),
+                    onTap: () => widget.onClose(),
+                  ),
+                  enabledBorder: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black12),
+                  ),
+                  focusedBorder: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black12),
+                  ),
+                  border: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black12),
+                  ),
                 ),
-              ],
+                onChanged: (v) async {
+                  final filteredList = _fonts.where((element) => element.contains(v)).toList();
+                  _filteredFonts.clear();
+                  _filteredFonts.addAll(filteredList);
+                  _loadedItems = _pageSize;
+                  _pagingController.refresh();
+                  setState(() {});
+                },
+              ),
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              itemCount: _filteredGoogleStyles.length,
-              itemBuilder: (ctx, i) {
-                final font = _filteredGoogleStyles[i];
-                return InkWell(
+            child: PagedListView<int, String>(
+              pagingController: _pagingController,
+              cacheExtent: 20,
+              builderDelegate: PagedChildBuilderDelegate<String>(
+                itemBuilder: (context, item, index) => InkWell(
                   child: Card(
+                    margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
                     child: Container(
                       margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
                       padding: const EdgeInsets.symmetric(vertical: 16),
@@ -104,13 +127,12 @@ class _FontsPickerState extends State<FontsPicker> {
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           Text(
-                            font.name,
-                            key: ValueKey(font.style),
-                            style: _prepareStyleForText(font),
+                            item,
+                            style: GoogleFonts.getFont(item),
                           ),
                           GestureDetector(
                             onTap: () async {
-                              final url = Uri.tryParse(ConstsUrl.font(font.name));
+                              final url = Uri.tryParse(ConstsUrl.font(item));
                               if (url != null) {
                                 if (await canLaunchUrl(url)) {
                                   await launchUrl(url);
@@ -121,25 +143,19 @@ class _FontsPickerState extends State<FontsPicker> {
                             },
                             child: Text(
                               context.l10n.feature_theme_edit_FontPicker_font_details,
-                              style: const TextStyle(color: Colors.blue, fontWeight: FontWeight.w100),
                             ),
                           ),
                         ],
                       ),
                     ),
                   ),
-                  onTap: () => widget.onChoose(font.name),
-                );
-              },
+                  onTap: () => widget.onChoose(''),
+                ),
+              ),
             ),
           )
         ],
       ),
     );
-  }
-
-  TextStyle? _prepareStyleForText(GoogleStyle fontStyle) {
-    final style = fontStyle.style();
-    return _filteredGoogleStyles.length < _renderedItemsStyleCount ? style : null;
   }
 }
