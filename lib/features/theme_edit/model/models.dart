a7220755359fb89a6a7df819ea1c3a86e9789b66
Dmitro Serdun <d.serdun@webtrit.com>
2023-03-17 15:22:59 +0200

    feat: remove unnecessary enum

--- lib/features/theme_edit/model/models.dart
+++ lib/features/theme_edit/model/models.dart
@@ -1,5 +1,4 @@
 export 'event_log_model.dart';
 export 'focus_model.dart';
-export 'screens_enum.dart';
 export 'theme_description_input.dart';
 export 'theme_name_input.dart';
--- lib/features/theme_edit/model/screens_enum.dart
+++
@@ -1,1 +0,0 @@
-enum ScreenEnum { auth, main, setting }
