b5606634a886834e6e47371ec612a0888ae93854
Dmitro Serdun <d.serdun@webtrit.com>
2023-03-29 10:00:23 +0300

    feat: add http datasource

--- lib/share/data/data.dart
+++ lib/share/data/data.dart
@@ -1,2 +1,3 @@
 export 'auth_firebase_data.dart';
+export 'http_datasource.dart';
 export 'realtime_firebase_data.dart';
---
+++ lib/share/data/http_datasource.dart
@@ -0,0 +1,59 @@
+import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
+
+import 'package:webtrit_configurator/core/config/env/app_environment.dart';
+import 'package:webtrit_configurator/share/entity/dto/dto.dart';
+
+@singleton
+class HttpDatasource {
+  final Dio dio;
+  final AppEnvironment environment;
+
+  HttpDatasource(this.dio, this.environment);
+
+  Future<List<VendorDTO>> getApplications() async {
+    final response = await dio.get('${environment.endpoints.url}/applications/');
+    return (response.data as List).map((data) => VendorDTO.fromJson(data)).toList();
+  }
+
+  Future<VendorDTO> createApplications(VendorDTO vendorDTO) async {
+    final response = await dio.post('${environment.endpoints.url}/applications/', data: vendorDTO.toJson());
+    return VendorDTO.fromJson(response.data);
+  }
+
+  Future<VendorDTO> updateApplications(VendorDTO application) async {
+    final response = await dio.patch('${environment.endpoints.url}/applications/', data: application.toJson());
+    return VendorDTO.fromJson(response.data);
+  }
+
+  Future<VendorDTO> deleteApplications(VendorDTO application) async {
+    final response = await dio.delete('${environment.endpoints.url}/applications/', data: application.toJson());
+    return VendorDTO.fromJson(response.data);
+  }
+
+  Future<List<ThemeDTO>> getThemes(String applicationId) async {
+    final response = await dio.get('${environment.endpoints.url}/applications/$applicationId/themes');
+    return (response.data as List).map((data) => ThemeDTO.fromJson(data)).toList();
+  }
+
+  Future<ThemeDTO> createTheme(String applicationId, ThemeDTO themeDTO) async {
+    final response = await dio.post('${environment.endpoints.url}/applications/$applicationId/themes');
+    return ThemeDTO.fromJson(response.data);
+  }
+
+  Future<ThemeDTO> updateTheme(String applicationId, ThemeDTO themeDTO) async {
+    final response = await dio.patch('${environment.endpoints.url}/applications/$applicationId/themes/${themeDTO.id}',
+        data: themeDTO.toJson());
+    return ThemeDTO.fromJson(response.data);
+  }
+
+  Future<ThemeDTO> getTheme(String applicationId, String themeId) async {
+    final response = await dio.get('${environment.endpoints.url}/applications/$applicationId/themes/$themeId');
+    return ThemeDTO.fromJson(response.data);
+  }
+
+  Future<ThemeDTO> deleteTheme(String applicationId, String themeId) async {
+    final response = await dio.delete('${environment.endpoints.url}/applications/$applicationId/themes/$themeId');
+    return ThemeDTO.fromJson(response.data);
+  }
+}
