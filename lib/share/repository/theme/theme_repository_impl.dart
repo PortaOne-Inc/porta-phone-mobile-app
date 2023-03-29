93e53b5dbfbda29ef58420c5c34565c909197d4b
Dmitro Serdun <d.serdun@webtrit.com>
2023-03-29 10:02:35 +0300

    feat: migrate to http datasource

--- lib/share/repository/theme/theme_repository_impl.dart
+++ lib/share/repository/theme/theme_repository_impl.dart
@@ -2,43 +2,22 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:injectable/injectable.dart';
 
 import 'package:webtrit_configurator/core/exception/exception.dart';
-
-import '../../data/data.dart';
-import '../../entity/dto/dto.dart';
-
-import 'theme_repository.dart';
+import 'package:webtrit_configurator/share/share.dart';
 
 @Injectable(as: ThemeRepository)
 class ThemeRepositoryImpl extends ThemeRepository {
   ThemeRepositoryImpl({
-    required this.datasource,
     required this.firebaseStorage,
+    required this.httpDatasource,
   });
 
-  final RealtimeFirebaseData datasource;
   final FirebaseStorage firebaseStorage;
-
-  final String _themesDirectoryName = 'themes';
-  final String _themeIdField = 'id';
-
-  String _getThemesPath(
-    String userId,
-    String applicationId,
-  ) =>
-      '$_themesDirectoryName/$userId/$applicationId/';
-
-  String _getThemePath({
-    required String userId,
-    required String applicationId,
-    required String themeId,
-  }) {
-    return '${_getThemesPath(userId, applicationId)}$themeId';
-  }
+  final HttpDatasource httpDatasource;
 
   @override
   Future<ThemeDTO> updateTheme(String userId, String applicationId, ThemeDTO? theme) async {
     try {
-      return await _tryUpdateTheme(userId, applicationId, theme);
+      return await httpDatasource.updateTheme(applicationId, theme!);
     } catch (e) {
       throw BaseException(message: e.toString());
     }
@@ -47,7 +26,7 @@ class ThemeRepositoryImpl extends ThemeRepository {
   @override
   Future<ThemeDTO> createTheme(String userId, String applicationId, ThemeDTO theme) async {
     try {
-      return await _tryCreateTheme(userId, applicationId, theme);
+      return await httpDatasource.createTheme(applicationId, theme);
     } catch (e) {
       throw BaseException(message: e.toString());
     }
@@ -56,7 +35,7 @@ class ThemeRepositoryImpl extends ThemeRepository {
   @override
   Future<List<ThemeDTO>> getThemes(String userId, String applicationId) async {
     try {
-      return await _tryGetThemes(userId, applicationId);
+      return httpDatasource.getThemes(applicationId);
     } catch (e) {
       throw BaseException(message: e.toString());
     }
@@ -65,7 +44,7 @@ class ThemeRepositoryImpl extends ThemeRepository {
   @override
   Future<ThemeDTO> getTheme(String userId, String applicationId, String themeId) async {
     try {
-      return await _tryGetTheme(userId, applicationId, themeId);
+      return await httpDatasource.getTheme(applicationId, themeId);
     } on NoContentException catch (_) {
       rethrow;
     } catch (e) {
@@ -76,59 +55,10 @@ class ThemeRepositoryImpl extends ThemeRepository {
   @override
   Future<ThemeDTO> deleteTheme(String userId, String applicationId, ThemeDTO themeDTO) async {
     try {
-      return await _tryDeleteTheme(userId, applicationId, themeDTO);
+      return httpDatasource.deleteTheme(applicationId, themeDTO.id!);
     } catch (e) {
       throw BaseException(message: e.toString());
     }
-  }
-
-  Future<ThemeDTO> _tryUpdateTheme(String userId, String applicationId, ThemeDTO? theme) async {
-    await datasource.patch(
-        _getThemePath(
-          userId: userId,
-          applicationId: applicationId,
-          themeId: theme!.id!,
-        ),
-        theme.toJson());
-    return theme;
-  }
-
-  Future<ThemeDTO> _tryCreateTheme(String userId, String applicationId, ThemeDTO theme) async {
-    final themeId = await datasource.post(_getThemesPath(userId, applicationId), theme.toJson());
-    return Future.value(theme.copyWith(id: themeId));
-  }
-
-  Future<List<ThemeDTO>> _tryGetThemes(String userId, String applicationId) async {
-    final themes = await datasource.get<String, Map<String, dynamic>>(_getThemesPath(userId, applicationId));
-    return themes.map((it) {
-      final raw = it.value..update(_themeIdField, (value) => it.key, ifAbsent: () => it.key);
-      return ThemeDTO.fromJson(raw);
-    }).toList();
-  }
-
-  Future<ThemeDTO> _tryGetTheme(String userId, String applicationId, String themeId) async {
-    final theme = await datasource.get<String, dynamic>(_getThemePath(
-      userId: userId,
-      applicationId: applicationId,
-      themeId: themeId,
-    ));
-
-    if (theme.isNotEmpty) {
-      final map = Map<String, dynamic>.fromEntries(theme);
-      final raw = map..update(_themeIdField, (value) => themeId, ifAbsent: () => themeId);
-      return ThemeDTO.fromJson(raw);
-    } else {
-      throw NoContentException();
-    }
-  }
-
-  Future<ThemeDTO> _tryDeleteTheme(String userId, String applicationId, ThemeDTO theme) async {
-    await datasource.delete(_getThemePath(
-      userId: userId,
-      applicationId: applicationId,
-      themeId: theme.id!,
-    ));
-    return Future.value(theme);
   }
 
   @override
--- lib/share/repository/vendor/vendor_repository_impl.dart
+++ lib/share/repository/vendor/vendor_repository_impl.dart
@@ -5,21 +5,16 @@ import 'package:webtrit_configurator/share/share.dart';
 
 @Injectable(as: VendorRepository)
 class VendorRepositoryImpl extends VendorRepository {
-  VendorRepositoryImpl({required this.datasource});
+  HttpDatasource httpDatasource;
 
-  final RealtimeFirebaseData datasource;
-
-  final String _themesDirectoryName = 'applications';
-  final String _appIdField = 'id';
-
-  String _getApplicationsPath(String userId) => '$_themesDirectoryName/$userId/';
-
-  String _getApplicationPath(String userId, String id) => '$_themesDirectoryName/$userId/$id';
+  VendorRepositoryImpl({
+    required this.httpDatasource,
+  });
 
   @override
   Future<VendorDTO> createApplication(String userId, VendorDTO applicationDTO) async {
     try {
-      return await _tryCreateApplication(applicationDTO, userId);
+      return await httpDatasource.createApplications(applicationDTO);
     } catch (e) {
       throw BaseException(message: e.toString());
     }
@@ -28,7 +23,7 @@ class VendorRepositoryImpl extends VendorRepository {
   @override
   Future<List<VendorDTO>> getUserApplications(String userId) async {
     try {
-      return await _tryGetUserApplications(userId);
+      return await httpDatasource.getApplications();
     } catch (e) {
       throw BaseException(message: e.toString());
     }
@@ -37,7 +32,7 @@ class VendorRepositoryImpl extends VendorRepository {
   @override
   Future<VendorDTO> deleteApplication(String userId, VendorDTO applicationDTO) async {
     try {
-      return _tryDeleteApplication(userId, applicationDTO);
+      return await httpDatasource.deleteApplications(applicationDTO);
     } catch (e) {
       throw BaseException(message: e.toString());
     }
@@ -46,34 +41,9 @@ class VendorRepositoryImpl extends VendorRepository {
   @override
   Future<VendorDTO> updateApplication(String userId, VendorDTO applicationDTO) async {
     try {
-      return _tryUpdateApplication(userId, applicationDTO);
+      return await httpDatasource.updateApplications(applicationDTO);
     } catch (e) {
       throw BaseException(message: e.toString());
     }
-  }
-
-  Future<VendorDTO> _tryCreateApplication(VendorDTO applicationDTO, String userId) async {
-    final inJson = applicationDTO.toJson();
-    final id = await datasource.post(_getApplicationsPath(userId), inJson);
-    final outJson = inJson..update(_appIdField, (value) => id, ifAbsent: () => id);
-    return VendorDTO.fromJson(outJson);
-  }
-
-  Future<List<VendorDTO>> _tryGetUserApplications(String userId) async {
-    final themes = await datasource.get<String, Map<String, dynamic>>(_getApplicationsPath(userId));
-    return themes.map((it) {
-      final raw = it.value..update(_appIdField, (value) => it.key, ifAbsent: () => it.key);
-      return VendorDTO.fromJson(raw);
-    }).toList();
-  }
-
-  Future<VendorDTO> _tryDeleteApplication(String userId, VendorDTO applicationDTO) async {
-    await datasource.delete(_getApplicationPath(userId, applicationDTO.id!));
-    return applicationDTO;
-  }
-
-  Future<VendorDTO> _tryUpdateApplication(String userId, VendorDTO applicationDTO) async {
-    await datasource.patch(_getApplicationPath(userId, applicationDTO.id!), applicationDTO.toJson());
-    return applicationDTO;
   }
 }
