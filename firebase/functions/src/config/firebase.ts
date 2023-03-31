2160092aaaa608bcdcd70a7946cd5769b1744d38
Dmitro Serdun <d.serdun@webtrit.com>
2023-03-31 19:05:19 +0300

    feat(firebase): update config

--- firebase/functions/src/config/firebase.ts
+++ firebase/functions/src/config/firebase.ts
@@ -1,20 +1,18 @@
+import * as firebase from "firebase/app"
 import * as admin from "firebase-admin"
 import * as functions from "firebase-functions"
 import * as firestore from "firebase-admin/firestore"
+import * as auth from "firebase/auth"
 
-import {
-    signInWithEmailAndPassword,
-    getAuth,
-    connectAuthEmulator,
+const firebaseConfig = require('../../../credentials/firebase_config.json');
+const serviceAccount = require('../../../credentials/service_account.json');
 
-} from 'firebase/auth'
+firebase.initializeApp(firebaseConfig);
 
-import {initializeApp} from 'firebase/app';
+if (process.env.FUNCTIONS_EMULATOR === 'true') {
+    auth.connectAuthEmulator(auth.getAuth(), "http://127.0.0.1:7980")
+}
 
-initializeApp();
-
-admin.initializeApp();
-
-
-const dbFirestore = admin.firestore()
-export {admin, functions, firestore, dbFirestore, signInWithEmailAndPassword, getAuth}
+admin.initializeApp(serviceAccount);
+const db = admin.firestore()
+export {admin, functions, firestore, db, auth}
