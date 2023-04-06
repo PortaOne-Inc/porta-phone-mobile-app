d9ba40caca047c7933de1c9e972dc25d538411fa
Dmitro Serdun <d.serdun@webtrit.com>
2023-04-06 15:08:14 +0300

    refactor: remove ResponsiveWrapper background color

--- lib/core/config/app/application_material.dart
+++ lib/core/config/app/application_material.dart
@@ -39,16 +39,17 @@ class MaterialApplication extends StatelessWidget {
             Locale('en', ''), // English, no country code
             Locale('ar', ''), // Spanish, no country code
           ],
-          builder: (context, widget) => ResponsiveWrapper.builder(BouncingScrollWrapper.builder(context, widget!),
-              minWidth: 450,
-              defaultScale: true,
-              debugLog: false,
-              breakpoints: [
-                const ResponsiveBreakpoint.autoScaleDown(800, name: TABLET),
-                const ResponsiveBreakpoint.autoScale(1000, name: TABLET),
-                const ResponsiveBreakpoint.resize(1200, name: DESKTOP),
-              ],
-              background: Container(color: const Color(0xFFF5F5F5))),
+          builder: (context, widget) => ResponsiveWrapper.builder(
+            BouncingScrollWrapper.builder(context, widget!),
+            minWidth: 450,
+            defaultScale: true,
+            debugLog: false,
+            breakpoints: [
+              const ResponsiveBreakpoint.autoScaleDown(800, name: TABLET),
+              const ResponsiveBreakpoint.autoScale(1000, name: TABLET),
+              const ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+            ],
+          ),
           debugShowCheckedModeBanner: false,
           restorationScopeId: 'App',
           routeInformationProvider: route.routeInformationProvider,
