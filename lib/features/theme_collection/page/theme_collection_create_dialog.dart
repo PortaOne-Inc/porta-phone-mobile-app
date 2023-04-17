e5d6038d75dbbfb9da846a9da0415e8c0d16cbbd
Dmitro Serdun <d.serdun@webtrit.com>
2023-04-17 15:13:17 +0300

    refactor: remove overhead widgets from theme collection create dialog

--- lib/features/theme_collection/page/theme_collection_create_dialog.dart
+++ lib/features/theme_collection/page/theme_collection_create_dialog.dart
@@ -40,56 +40,51 @@ class _ThemeCollectionCreateDialogState extends State<ThemeCollectionCreateDialo
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: ColorField(
-                    title: context.l10n.configurator_color_primary,
-                    color: _selectedColor,
-                    onTap: _onSelectColor,
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      _isDefault = !_isDefault;
-                    });
-                  },
-                  child: Card(
-                    elevation: _isDefault ? 2 : 0.75,
-                    margin: const EdgeInsets.only(left: 8),
-                    child: Tooltip(
-                      message: context.l10n.feature_theme_as_default,
-                      child: const SizedBox(
-                        width: 56,
-                        height: 56,
-                        child: Icon(
-                          Icons.radio_button_checked,
-                          color: Colors.green,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+            Text(
+              context.l10n.feature_theme_create_title,
+              style: Theme.of(context).textTheme.titleMedium,
             ),
             const SizedBox(
               height: 8,
             ),
-            TextFormField(
-              controller: _controller,
-              decoration: InputDecoration(
-                hintText: context.l10n.theme_name,
-                errorText: _themeNameInput?.errorL10n(context),
+            Row(children: [
+              Expanded(
+                child: TextFormField(
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    hintText: context.l10n.theme_name,
+                    errorText: _themeNameInput?.errorL10n(context),
+                  ),
+                  onChanged: (it) {
+                    setState(() {
+                      _themeNameInput = ThemeNameInput.dirty(it);
+                    });
+                  },
+                ),
               ),
-              onChanged: (it) {
-                setState(() {
-                  _themeNameInput = ThemeNameInput.dirty(it);
-                });
-              },
-            ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _isDefault = !_isDefault;
+                  });
+                },
+                child: Card(
+                  elevation: _isDefault ? 2 : 0.75,
+                  margin: const EdgeInsets.only(left: 8),
+                  child: Tooltip(
+                    message: context.l10n.feature_theme_as_default,
+                    child: const SizedBox(
+                      width: 56,
+                      height: 56,
+                      child: Icon(
+                        Icons.radio_button_checked,
+                        color: Colors.green,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ]),
             const SizedBox(
               height: 16,
             ),
@@ -104,26 +99,5 @@ class _ThemeCollectionCreateDialogState extends State<ThemeCollectionCreateDialo
         ),
       ),
     );
-  }
-
-  void _onSelectColor(Color color) async {
-    _selectedColor = await showDialog(
-      context: context,
-      barrierColor: Colors.transparent,
-      builder: (context) => Dialog(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        insetPadding: const EdgeInsets.symmetric(horizontal: 64),
-        child: ConstrainedBox(
-          constraints: BoxConstraints(maxHeight: MediaQuery.of(context).size.height),
-          child: ColorPicker(
-            onDeclineColor: () => Navigator.of(context).pop(),
-            onAcceptColor: (color) => Navigator.of(context).pop(color),
-            initialColor: color,
-          ),
-        ),
-      ),
-    );
-    setState(() {});
   }
 }
--- lib/features/theme_collection/widgets/item_theme.dart
+++ lib/features/theme_collection/widgets/item_theme.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:webtrit_configurator/core/l10n/l10n.dart';
-import 'package:webtrit_configurator/core/widgets/widgets.dart';
 import 'package:webtrit_configurator/share/share.dart';
 
 import 'used_color.dart';
