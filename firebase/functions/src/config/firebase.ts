4c2a04727481be7c64352852d03b121185f0747d
Dmitro Serdun <d.serdun@webtrit.com>
2023-04-21 17:32:48 +0300

    refactor(firebase):  add debug env

--- firebase/functions/src/config/firebase.ts
+++ firebase/functions/src/config/firebase.ts
@@ -1,8 +1,17 @@
 import * as admin from 'firebase-admin'
 import * as functions from 'firebase-functions'
 import * as firestore from 'firebase-admin/firestore'
+import * as auth from 'firebase/auth'
+import * as firebase from 'firebase/app'
 
-admin.initializeApp();
+if (process.env.FUNCTIONS_EMULATOR === 'true') {
+	firebase.initializeApp(require('../../../credentials/firebase_config.json'));
+	auth.connectAuthEmulator(auth.getAuth(), 'http://127.0.0.1:7980')
+	admin.initializeApp(require('../../../credentials/service_account.json'));
+} else {
+	admin.initializeApp();
+}
+
 const database = admin.firestore()
 
-export {admin, functions, firestore, database}
+export {admin, functions, firestore, database, auth}
