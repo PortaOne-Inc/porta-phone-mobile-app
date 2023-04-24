2c981a6d6c82d6f8954f85ecc0f14a6f3c3930d4
Dmitro Serdun <d.serdun@webtrit.com>
2023-04-24 09:10:51 +0300

    refactor(firebase): change firebase config env

--- firebase/functions/src/config/firebase.ts
+++ firebase/functions/src/config/firebase.ts
@@ -1,17 +1,20 @@
 import * as admin from 'firebase-admin'
 import * as functions from 'firebase-functions'
 import * as firestore from 'firebase-admin/firestore'
-import * as auth from 'firebase/auth'
-import * as firebase from 'firebase/app'
 
-if (process.env.FUNCTIONS_EMULATOR === 'true') {
-	firebase.initializeApp(require('../../../credentials/firebase_config.json'));
-	auth.connectAuthEmulator(auth.getAuth(), 'http://127.0.0.1:7980')
-	admin.initializeApp(require('../../../credentials/service_account.json'));
-} else {
-	admin.initializeApp();
-}
+// TODO: Add auto env detecting
+// import * as auth from "firebase/auth"
+// import * as firebase from "firebase/app"
 
+// const firebaseConfig = require('../../../credentials/firebase_config.json');
+// const serviceAccount = require('../../../credentials/service_account.json');
+
+// firebase.initializeApp(firebaseConfig);
+
+// auth.connectAuthEmulator(auth.getAuth(), 'http://127.0.0.1:7980')
+// admin.initializeApp(serviceAccount);
+
+
+admin.initializeApp();
 const database = admin.firestore()
-
-export {admin, functions, firestore, database, auth}
+export {admin, functions, firestore, database}
