b4e352d6c09d21ec7738c078df73a3288821ddc0
Dmitro Serdun <d.serdun@webtrit.com>
2023-04-28 01:08:48 +0300

    refactor: remove deprecated files

--- firebase/functions/src/controllers/triggers/database/themes.ts
+++
@@ -1,7 +0,0 @@
-import {functions} from '../../../config/firebase'
-
-export const onThemeCreated = functions.firestore
-	.document('themes/{id}')
-	.onCreate(async (snap, context) => {
-		return null;
-	});
--- firebase/functions/src/middleware/middleware.ts
+++
@@ -1,4 +0,0 @@
-export * from './auth/auth_verefication'
-export * from './mapping/body_id_ignore';
-export * from './validation/application_validation'
-export * from './validation/theme_validation'
