abba9e77cf03c58a4fff686a890335fd0e7e626a
Dmitro Serdun <d.serdun@webtrit.com>
2023-05-07 22:32:01 +0300

    feat(firebase): add delete theme endpoint

--- firebase/functions/src/features/themes/repository/themes.repository.interface.ts
+++ firebase/functions/src/features/themes/repository/themes.repository.interface.ts
@@ -3,6 +3,7 @@ import Theme from '../../../core/models/theme';
 export default interface IThemeRepository {
 	create: (application: Theme) => Promise<Theme>;
 	patch: (application: Theme) => Promise<Theme>;
-	getByThemeId: (applicationId: string, id: string) => Promise<Theme | null>;
+	delete: (themeId: string) => Promise<void | null>;
+	get: (id: string) => Promise<Theme | null>;
 	getByApplicationsId: (id: string) => Promise<Theme[]>;
 }
--- firebase/functions/src/features/themes/service/themes.service.ts
+++ firebase/functions/src/features/themes/service/themes.service.ts
@@ -4,6 +4,7 @@ import {inject, injectable} from 'inversify';
 
 import IThemeRepository from '../repository/themes.repository.interface';
 import IThemesService from './themes.service.interface';
+import IApplicationRepository from '../../applications/repository/application.repository.interface';
 
 import {TYPES} from '../../../di';
 
@@ -13,7 +14,8 @@ import Theme from '../../../core/models/theme';
 @injectable()
 export default class ThemesService implements IThemesService {
 	constructor(
-		@inject(TYPES.ThemeRepository) private themeRepository: IThemeRepository
+		@inject(TYPES.ThemeRepository) private themeRepository: IThemeRepository,
+		@inject(TYPES.ApplicationRepository) private applicationRepository: IApplicationRepository
 	) {
 	}
 
@@ -23,7 +25,7 @@ export default class ThemesService implements IThemesService {
 	}
 
 	getByThemeId(applicationId: string, id: string): Promise<Theme | null> {
-		return this.themeRepository.getByThemeId(applicationId, id);
+		return this.themeRepository.get(id);
 	}
 
 	getByApplicationId(id: string): Promise<Theme[]> {
@@ -38,5 +40,20 @@ export default class ThemesService implements IThemesService {
 
 	patchTheme(theme: Theme, userDto: User): Promise<Theme | null> {
 		return this.themeRepository.patch(theme);
+	}
+
+	async deleteTheme(applicationId: string, themeId: string, userDto: User): Promise<void | null> {
+		try {
+			const application = await this.applicationRepository.getById(applicationId);
+			const theme = await this.themeRepository.get(themeId);
+
+			if (theme?.applicationId == application?.id && application?.user == userDto.uid) {
+				return this.themeRepository.delete(themeId);
+			} else {
+				return null;
+			}
+		} catch (e) {
+			return null;
+		}
 	}
 }
