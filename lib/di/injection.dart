2f7a9abcf1b34ee9a9a591a404c7aacbd4abfa21
Dmitro Serdun <d.serdun@webtrit.com>
2023-05-09 11:12:38 +0300

    refactor: remove unused dependency

--- lib/di/injection.dart
+++ lib/di/injection.dart
@@ -1,6 +1,5 @@
 import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
@@ -21,9 +20,6 @@ Future<GetIt> configureDependencies({
 
 @module
 abstract class RegisterModule {
-
-  @LazySingleton()
-  FirebaseDatabase database() => FirebaseDatabase.instance;
 
   @LazySingleton()
   FirebaseAuth auth() {
