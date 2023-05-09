dc2c06869d41d9d88220cae28551f0d7fe5f674c
Dmitro Serdun <d.serdun@webtrit.com>
2023-05-09 08:12:33 +0300

    refactor: add duration to mixin messages

--- lib/share/mixin/mixin_messages.dart
+++ lib/share/mixin/mixin_messages.dart
@@ -3,13 +3,18 @@ import 'package:flutter/material.dart';
 import '../widgets/widgets.dart';
 
 mixin MixinMessages {
-  void showTopSnakeMessageSuccess(BuildContext context, String message, {Color color = Colors.lightGreen}) {
+  void showTopSnakeMessageSuccess(
+    BuildContext context,
+    String message, {
+    Color color = Colors.lightGreen,
+    Duration duration = const Duration(milliseconds: 500),
+  }) {
     final snakeBar = SnackBar(
         content: Text(message, textAlign: TextAlign.center),
         behavior: SnackBarBehavior.floating,
         backgroundColor: color,
         dismissDirection: DismissDirection.up,
-        duration: const Duration(milliseconds: 500),
+        duration: duration,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
         margin: EdgeInsets.only(bottom: MediaQuery.of(context).size.height - kToolbarHeight, right: 8, left: 8));
 
