c4fe1378869a898589a4f19d15d673c2e5b14a89
Dmitro Serdun <d.serdun@webtrit.com>
2023-05-15 16:28:16 +0300

    feat: add firebase config

---
+++ web/firebase-messaging-sw.js
@@ -0,0 +1,12 @@
+importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+   apiKey: "AIzaSyDfVipSr47hmDzE-b4Xq9y3ozFSF-GgLdc",
+    authDomain: "webtrit-configurator-stage.firebaseapp.com",
+    projectId: "webtrit-configurator-stage",
+    storageBucket: "webtrit-configurator-stage.appspot.com",
+    messagingSenderId: "353614723935",
+    appId: "1:353614723935:web:9e515b8043a2f84e6b54ea",
+    measurementId: "G-M2NNXXL0PY",
+});
