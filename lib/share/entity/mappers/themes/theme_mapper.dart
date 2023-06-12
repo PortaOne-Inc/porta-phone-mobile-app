29bcadeae87e40a6b101870562afe24f0e54cb28
Dmitro Serdun <d.serdun@webtrit.com>
2023-06-12 14:10:09 +0300

    refactor: remove unused imports

--- lib/share/entity/mappers/themes/theme_mapper.dart
+++ lib/share/entity/mappers/themes/theme_mapper.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/material.dart';
-
 import 'package:injectable/injectable.dart';
 
 import '../../dto/dto.dart';
--- lib/share/entity/models/application/application_model.dart
+++ lib/share/entity/models/application/application_model.dart
@@ -1,10 +1,4 @@
-import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:webtrit_configurator/share/exports/exports.dart';
-
-import '../color/color_scheme_model.dart';
-import '../image/image_scheme_model.dart';
-import '../texts/texts_model.dart';
 
 part 'application_model.freezed.dart';
 
