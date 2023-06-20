94b34bf1b596d1da462a24c73d752231d410764c
Dmitro Serdun <d.serdun@webtrit.com>
2023-06-20 18:51:43 +0300

    fix: change resizable widget logic

--- lib/features/theme_edit/page/page_theme_edit.dart
+++ lib/features/theme_edit/page/page_theme_edit.dart
@@ -13,7 +13,7 @@ import '../widgets/widgets.dart';
 
 import 'page_theme_preview.dart';
 
-class PageThemeEdit extends StatelessWidget with MixinMessages {
+class PageThemeEdit extends StatefulWidget with MixinMessages {
   PageThemeEdit({
     super.key,
     required this.title,
@@ -21,7 +21,13 @@ class PageThemeEdit extends StatelessWidget with MixinMessages {
 
   final String title;
 
+  @override
+  State<PageThemeEdit> createState() => _PageThemeEditState();
+}
+
+class _PageThemeEditState extends State<PageThemeEdit> {
   final _leftPageNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'Left edit theme page');
+
   final _rightPageNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'Right edit theme page');
 
   @override
@@ -32,7 +38,7 @@ class PageThemeEdit extends StatelessWidget with MixinMessages {
         appBar: BaseToolBar(
           isVisibleProgress: state is ThemePropertyProgressState,
           child: ThemesEditToolbar(
-            title: title,
+            title: widget.title,
             onSaveTheme: () => _updateTheme(context),
             onLogout: () => BlocProvider.of<CommonBloc>(context).logout(),
             onPreload: () => _openTemplates(state),
--- lib/share/widgets/behaviour/background_binary_resizable_column.dart
+++ lib/share/widgets/behaviour/background_binary_resizable_column.dart
@@ -11,6 +11,7 @@ class BackgroundBinaryResizableColumn extends StatefulWidget {
 
   final Widget leftChild;
   final Widget rightChild;
+
   final double minColumWidth;
   final double minScrollWidth;
 
@@ -19,19 +20,27 @@ class BackgroundBinaryResizableColumn extends StatefulWidget {
 }
 
 class _BackgroundBinaryResizableColumnState extends State<BackgroundBinaryResizableColumn> {
-  final double _columnMargin = 2;
+  final _columnMargin = 2.0;
 
-  double _screenWidth = 0;
-  double _leftColumnWidth = 0;
-  double _rightColumnWidth = 0;
-  double _verticalDividerPosition = 0;
+  var _screenWidth = 0.0;
+  var _leftColumnWidth = 0.0;
+  var _rightColumnWidth = 0.0;
+  var _horizontalDividerPosition = 0.0;
 
   @override
   void didChangeDependencies() {
-    _screenWidth = MediaQuery.of(context).size.width - _columnMargin;
-    _leftColumnWidth = _calculateLeftContainerWidth(0);
-    _rightColumnWidth = _calculateRightContainerWidth(0);
+    _recalculateProportion();
     super.didChangeDependencies();
+  }
+
+  double _getScreenWidth() {
+    return MediaQuery.of(context).size.width - _columnMargin;
+  }
+
+  void _recalculateProportion() {
+    _screenWidth = _getScreenWidth();
+    _leftColumnWidth = _calculateLeftContainerWidth(_horizontalDividerPosition);
+    _rightColumnWidth = _calculateRightContainerWidth(_horizontalDividerPosition);
   }
 
   @override
@@ -98,9 +107,9 @@ class _BackgroundBinaryResizableColumnState extends State<BackgroundBinaryResiza
   }
 
   void _reCalculateChildrenWidth(DragUpdateDetails details) {
-    _verticalDividerPosition += details.delta.dx;
-    _leftColumnWidth = _calculateLeftContainerWidth(_verticalDividerPosition);
-    _rightColumnWidth = _calculateRightContainerWidth(_verticalDividerPosition);
+    _horizontalDividerPosition += details.delta.dx;
+    _leftColumnWidth = _calculateLeftContainerWidth(_horizontalDividerPosition);
+    _rightColumnWidth = _calculateRightContainerWidth(_horizontalDividerPosition);
 
     setState(() {});
   }
