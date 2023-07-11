63751672d93be6f8ce700e7eccfaa1888390fb39
Dmitro Serdun <d.serdun@webtrit.com>
2023-07-11 15:41:17 +0300

    feat(firebase): add launch color model

--- firebase/functions/src/core/models/colors.ts
+++ firebase/functions/src/core/models/colors.ts
@@ -1,15 +1,18 @@
+import Launch from "./launch";
+
 export default class Colors {
-	public onPrimary?: string;
-	public onSurface?: string;
-	public surface?: string;
-	public onSecondaryContainer?: string;
-	public secondaryContainer?: string;
-	public tertiary?: string;
-	public error?: string;
-	public secondary?: string;
-	public outline?: string;
-	public background?: string;
-	public onBackground?: string;
-	public gradientTabColor?: string[];
-	public primary?: string;
+    public onPrimary?: string;
+    public onSurface?: string;
+    public surface?: string;
+    public onSecondaryContainer?: string;
+    public secondaryContainer?: string;
+    public tertiary?: string;
+    public error?: string;
+    public secondary?: string;
+    public outline?: string;
+    public background?: string;
+    public onBackground?: string;
+    public gradientTabColor?: string[];
+    public primary?: string;
+    public launch?: Launch;
 }
