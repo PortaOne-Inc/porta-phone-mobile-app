6499eda4075edd28b6b10f9885efcc8a39e28619
Dmitro Serdun <d.serdun@webtrit.com>
2023-07-31 11:04:33 +0300

    refactor: replace model source

--- lib/share/entity/dto/dto.dart
+++ lib/share/entity/dto/dto.dart
@@ -1,6 +1,1 @@
-export '../dto/application/application_dto.dart';
-export '../dto/color/color_dto.dart';
-export '../dto/image/image_collection_dto.dart';
-export '../dto/image/image_dto.dart';
-export '../dto/texts/texts.dart';
-export '../dto/theme/theme_dto.dart';
+export 'package:style/src/dto/dto.dart';
