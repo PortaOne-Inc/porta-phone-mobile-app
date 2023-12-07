f2a4120b1ed1b9856f1fd0f47cbf9259471c16e7
Dmitro Serdun <d.serdun@webtrit.com>
2023-12-07 14:37:51 +0200

    refactor: move dto package to common repository

--- packages/dto/lib/dto.dart
+++
@@ -1,6 +0,0 @@
-export 'application/application_dto.dart';
-export 'color/color_dto.dart';
-export 'image/image_collection_dto.dart';
-export 'image/image_dto.dart';
-export 'texts/texts.dart';
-export 'theme/theme_dto.dart';
