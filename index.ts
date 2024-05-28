6ffbaaedb6c601619fc09185e9c17febc951c86b
Dmitro Serdun <d.serdun@webtrit.com>
2024-05-28 22:49:09 +0300

    style: clean code

--- index.ts
+++ index.ts
@@ -4,25 +4,27 @@ import * as express from 'express';
 import * as functions from 'firebase-functions';
 import { AppModule } from './src/app.module';
 import { CONFIG } from './config';
+import { Express } from 'express-serve-static-core';
 
 const expressServer = express();
 
-const createFunction = async (expressInstance): Promise<void> => {
+const createFunction = async (expressInstance: Express): Promise<void> => {
   const app = await NestFactory.create(
     AppModule,
     new ExpressAdapter(expressInstance),
   );
 
+  app.setGlobalPrefix('v1');
   app.enableCors({
     origin: CONFIG.origin,
     methods: CONFIG.corsMethods,
     allowedHeaders: CONFIG.corsAllowedHeaders,
   });
-  await app.setGlobalPrefix('v1');
   await app.init();
 };
 
-// TODO(SERDUN): Change this variable name to 'api' after migrating to nest all features
+// TODO: Change this variable name to 'api' after migrating to nest all features
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const newapi = functions.https.onRequest(async (request, response) => {
   await createFunction(expressServer);
   expressServer(request, response);
