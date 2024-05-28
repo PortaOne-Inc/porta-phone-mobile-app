391e6614c0085f56f747ee9e10b2d8fcb8248c2b
Dmitro Serdun <d.serdun@webtrit.com>
2024-05-28 18:23:58 +0300

    Refactor/update path (#2)
    * feat: add global endpoints v1 path
    
    * refactor: change path prefix to new-api
    
    * style: format index.ts

--- index.ts
+++ index.ts
@@ -3,15 +3,21 @@ import { ExpressAdapter } from '@nestjs/platform-express';
 import * as express from 'express';
 import * as functions from 'firebase-functions';
 import { AppModule } from './src/app.module';
+
 const expressServer = express();
+
 const createFunction = async (expressInstance): Promise<void> => {
   const app = await NestFactory.create(
     AppModule,
     new ExpressAdapter(expressInstance),
   );
-await app.init();
+
+  await app.setGlobalPrefix('v1');
+  await app.init();
 };
-export const rest = functions.https.onRequest(async (request, response) => {
+
+// TODO(SERDUN): Change this variable name to 'api' after migrating to nest all features
+export const new-api = functions.https.onRequest(async (request, response) => {
   await createFunction(expressServer);
   expressServer(request, response);
 });
